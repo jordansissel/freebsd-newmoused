38d1fefc84994e193021581f0315d348f0e43868
Jordan Sissel <jls@semicomplete.com>
2005-08-13 20:47:58 +0000

    - Basic module framework implemented using dlopen(3)
    git-svn-id: svn+ssh://syn.csh.rit.edu/home/jls/backup/svn/newmoused@58 0c113055-a7d8-dd11-a243-00e0815b6b4e

--- moused/moused.c
+++ moused/moused.c
@@ -15,15 +15,23 @@ __FBSDID("Happy Cakes!");
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dlfcn.h>
 
 /* sysmouse stuff */
 #include <sys/mouse.h>
 #include <sys/consio.h>
 
+/* Don't include things needed by module libraries */
+#define THIS_IS_MOUSED 1
+
+#include "moused.h"
+
 static int verbose = 0;
 
 static void logmsg(int level, int errnum, const char *fmt, ...);
 static int moused(int argc, char **argv);
+static void loadmodule(char *path);
+static void update(struct mouse_info *delta);
 
 #define debug(level, fmt, args...) do {                        \
 	if (level <= verbose)                                       \
@@ -35,16 +43,14 @@ static int moused(int argc, char **argv);
 	exit(e);                                                    \
 } while (0)
 
-#define MOUSED_OPTIONS "p:m:f:"
+#define MOUSED_OPTIONS "d:m:f:"
 
-static struct rodentparam {
-	char *portname;
-	char *modulename;
-	char *configfile;
-} rodent = {
-	.portname = NULL,
+static struct rodentparam rodent = {
+	.device = NULL,
 	.modulename = NULL,
 	.configfile = "/etc/moused.conf",
+	.cfd = 0,
+	.updatefunc = update,
 };
 
 int main(int argc, char **argv) {
@@ -52,8 +58,8 @@ int main(int argc, char **argv) {
 
 	while (-1 != (c = getopt(argc, argv, MOUSED_OPTIONS))) {
 		switch (c) {
-			case 'p':
-				rodent.portname = optarg; break;
+			case 'd':
+				rodent.device = optarg; break;
 			case 'm':
 				rodent.modulename = optarg; break;
 			case 'f':
@@ -64,21 +70,33 @@ int main(int argc, char **argv) {
 	argc -= optind;
 	argv += optind;
 
+	if (NULL == rodent.device)
+		rodent.device = "/dev/psm0";
+
 	moused(argc, argv);
 
 	return 0;
 }
 
 int moused(int argc, char **argv) {
-	int mousefd;
 	struct mouse_info mouse;
-	int extioctl = 0;
-	int c;
 
-	if (-1 == (mousefd = open("/dev/consolectl", O_RDWR, 0))) {
+	if (-1 == (rodent.cfd = open("/dev/consolectl", O_RDWR, 0))) {
 		logfatal(1, "Unable to open /dev/consolectl");
 	}
 
+	if (NULL != rodent.modulename) {
+		char path[256];
+		snprintf(path, 256, "modules/%s/libmoused_%s.so", 
+					rodent.modulename, rodent.modulename);
+		printf("Path: %s\n", path);
+		loadmodule(path);
+		rodent.init(&rodent);
+		if (rodent.probe(&rodent) == MODULE_PROBE_SUCCESS) {
+			/* MAIN FUNCTION */
+			rodent.run(&rodent);
+		}
+	}
 }
 
 static void logmsg(int log_pri, int errnum, const char *fmt, ...) {
@@ -96,4 +114,20 @@ static void logmsg(int log_pri, int errnum, const char *fmt, ...) {
 
 	/* Syslog if we're in the background? */
 	warnx("%s", buf);
+}
+
+static void loadmodule(char *path) {
+	void *handle;
+
+	if (NULL == (handle = dlopen(path, RTLD_LAZY))) {
+		logfatal(1, "Unable open module '%s'.", rodent.modulename);
+	}
+
+	rodent.init = dlsym(handle, "init");
+	rodent.probe = dlsym(handle, "probe");
+	rodent.run = dlsym(handle, "run");
+}
+
+static void update(struct mouse_info *delta) {
+	ioctl(rodent.cfd, CONS_MOUSECTL, delta);
 }
