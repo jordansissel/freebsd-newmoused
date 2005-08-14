b99bfc800a66771572c14c7003ca9d6686ad458d
Jordan Sissel <jls@semicomplete.com>
2005-08-14 19:45:00 +0000

    - Moved some useful parts that moused.c and plugin modules will need here.   This is primarily the define for rodent_t and some macros to generate function headers.
    git-svn-id: svn+ssh://syn.csh.rit.edu/home/jls/backup/svn/newmoused@64 0c113055-a7d8-dd11-a243-00e0815b6b4e

---
+++ moused/moused.h
@@ -0,0 +1,36 @@
+
+#ifndef MOUSED_H
+#define MOUSED_H
+
+struct rodentparam;
+typedef struct rodentparam rodent_t;
+
+
+struct rodentparam {
+	char *device;
+	char *modulename;
+	char *configfile;
+	int cfd;
+
+	/* Modules will call this to pass deltas to sysmouse(4) */
+	int (*update)(struct mouse_info *);
+
+	/* Module callbacks (called by moused) */
+	int (*init)(rodent_t *, int, char **);
+	int (*probe)(rodent_t *);
+	int (*run)(rodent_t *);
+};
+
+#define MOUSED_INIT_FUNC  int init(rodent_t *rodent, int argc, char **argv)
+#define MOUSED_PROBE_FUNC int probe(rodent_t *rodent)
+#define MOUSED_RUN_FUNC   void run(rodent_t *rodent)
+
+#ifndef THIS_IS_MOUSED
+//static void (*update)(struct rodentparam *);
+#endif
+
+/* Probe function return values */
+#define MODULE_PROBE_FAIL    0
+#define MODULE_PROBE_SUCCESS 1
+
+#endif
