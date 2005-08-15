b91ea699d4c20a08f92cc0ffaf8dab67738a9103
Jordan Sissel <jls@semicomplete.com>
2005-08-15 02:51:21 +0000

    - sysmouse module supports both sysmouse and mousesystems protocols
    git-svn-id: svn+ssh://syn.csh.rit.edu/home/jls/backup/svn/newmoused@83 0c113055-a7d8-dd11-a243-00e0815b6b4e

--- moused/modules/sysmouse/moused_sysmouse.c
+++ moused/modules/sysmouse/moused_sysmouse.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/mouse.h>
 #include <sys/consio.h>
+#include <unistd.h>
 #include <fcntl.h>
 
 #include <stdio.h>
@@ -19,8 +20,29 @@ static void (*logmsg)(int, int, const char *, ...) = NULL;
 static void activity(rodent_t *rodent, char *packet);
 static void printbits(char packet);
 
+static int mouseproto = MOUSE_PROTO_SYSMOUSE;
+
 MOUSED_INIT_FUNC {
+	int c;
+	char *type = "sysmouse";
 	logmsg = rodent->logmsg;
+
+	while (-1 != (c = getopt(argc, argv, "t:"))) {
+		switch (c) {
+			case 't':
+				type = optarg; break;
+		}
+	}
+
+	if (strcmp(type, "sysmouse") == 0) {
+		mouseproto = MOUSE_PROTO_SYSMOUSE;
+	} else if ((strcmp(type, "mousesystems") == 0)
+				  || (strcmp(type, "msc") == 0)) {
+		mouseproto = MOUSE_PROTO_MSC;
+	} else {
+		warnx("Unknown protocol '%s' - defaulting to sysmouse", type);
+	}
+
 }
 
 MOUSED_PROBE_FUNC {
@@ -34,10 +56,10 @@ MOUSED_PROBE_FUNC {
 		/* Set the driver operation level */
 		ioctl(rodent->mfd, MOUSE_SETLEVEL, &level);
 		ioctl(rodent->mfd, MOUSE_GETMODE, &(rodent->mode));
-		if (MOUSE_PROTO_MSC == rodent->mode.protocol)
+		if (mouseproto == rodent->mode.protocol)
 			break;
 	}
-	if (MOUSE_PROTO_MSC != rodent->mode.protocol)
+	if (mouseproto != rodent->mode.protocol)
 		return MODULE_PROBE_FAIL;
 
 	return MODULE_PROBE_SUCCESS;
@@ -45,20 +67,31 @@ MOUSED_PROBE_FUNC {
 
 MOUSED_RUN_FUNC {
 	char *packet;
+	fd_set fds;
 
 	ioctl(rodent->mfd, MOUSE_GETHWINFO, &(rodent->hw));
 	printf("NumButtons: %d\n", rodent->hw.buttons);
 
 	packet = malloc(rodent->mode.packetsize);
 
+	FD_ZERO(&fds);
+	FD_SET(rodent->mfd, &fds);
+
 	for (;;) {
-		int bytes, x;
-		bytes = read(rodent->mfd, packet, rodent->mode.packetsize);
-		if (bytes < 0)
-			logfatal(1, "Error reading from mousey");
+		int tries = 0;
+		int bytes, x, c;
+
+		c = select(FD_SETSIZE, &fds, NULL, NULL, NULL); /* wait forever */
+		if (c < 0) {
+			logfatal(1, "Error with select call");
+		} else if (c > 0) {
+			bytes = read(rodent->mfd, packet, rodent->mode.packetsize); 
+			if (bytes < 0)
+				logfatal(1, "Error reading from mousey");
 		
-		if (bytes == rodent->mode.packetsize)
-			activity(rodent, packet);
+			if (bytes == rodent->mode.packetsize)
+				activity(rodent, packet);
+		}
 	}
 }
 
@@ -70,14 +103,12 @@ static void activity(rodent_t *rodent, char *packet) {
 	delta.u.data.buttons = 0;
 	delta.u.data.x = delta.u.data.y = delta.u.data.z = 0;
 
-	/*
 	for (x = 0; x < 8; x++)
 		printf("%02x ", (*(packet + x) & 0xff));
 	printf("\n");
-	*/
-	//printbits(*packet);
+	printbits(*packet);
 
-	/* mouse(4) details what is in the 5-byte packets */
+	/* sysmouse(4) details what is in the 8-byte packets */
 
 	delta.u.data.buttons |= NBIT(*packet, 3) 
 		                     | NBIT(*packet, 2) << 1
@@ -89,8 +120,12 @@ static void activity(rodent_t *rodent, char *packet) {
 	/* Bytes 3 and 5 are vertical */
 	delta.u.data.y = 0 - (*(packet + 2) + *(packet + 4)); 
 
-	//printf("(%d,%d) ", delta.u.data.x, delta.u.data.y);
-	//printbits(delta.u.data.buttons);
+	/* sysmouse adds z-axis changes */
+	if (MOUSE_PROTO_SYSMOUSE == mouseproto)
+		delta.u.data.z = (*(packet + 5) + *(packet + 6));
+
+	printf("(%d,%d) ", delta.u.data.x, delta.u.data.y);
+	printbits(delta.u.data.buttons);
 
 	rodent->update(&delta);
 }
