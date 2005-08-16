7d748ab703431a2dd4d59bb173b6f50249bcd882
Jordan Sissel <jls@semicomplete.com>
2005-08-16 05:42:49 +0000

    - Added 'packet-mode' where moused will loop waiting for data (using   select(2)). When data is available, rodent.handler() is called.   Handler is defined by the loaded module. - Wrote a basic filter for enabling virtual scrolling   (a patch I wrote for the existing moused(8)). Look at the diff,   writing this functionality is SOOOO much easier in this new moused than   in the older one, since I don't have to bounce around between functions   and control statements trying to figure out when it is proper to take   action for virtual scrolling. - update() function was updated to support a kind-of noop. If the   select(2) times-out, then update(NULL) is called. This is so filters can   be called when a timeout occurs and react accordingly, if necessary. - Example filter in the 'filter' function. This will probably be moved   to loadable modules later on, if there's enough reason to move this   elsewhere. - packethandler_main() is the main function where packet-mode exists.
    git-svn-id: svn+ssh://syn.csh.rit.edu/home/jls/backup/svn/newmoused@88 0c113055-a7d8-dd11-a243-00e0815b6b4e

--- moused/moused.c
+++ moused/moused.c
@@ -15,6 +15,7 @@ __FBSDID("Happy Cakes!");
 #include <fcntl.h>
 #include <unistd.h>
 #include <dlfcn.h>
+#include <sys/select.h>
 
 /* sysmouse stuff */
 #include <sys/mouse.h>
@@ -26,8 +27,18 @@ static int verbose = 0;
 
 static void logmsg(int log_pri, int errnum, const char *fmt, ...);
 static int moused(int argc, char **argv);
+
+/* Load a module into memory (dlopen, etc) */
 static void loadmodule(char *path);
-static int update(struct mouse_info *delta);
+
+/* Modules will call this function when the mouse moves/changes */
+static int update(mouse_info_t *delta);
+
+/* Filter the input somehow? */
+static void filter(mouse_info_t *delta, int noop);
+
+static void packethandler_main();
+void dumpstate(mouse_info_t *delta);
 
 #define debug(level, fmt, args...) do {                        \
 	if (level <= verbose)                                       \
@@ -40,9 +51,16 @@ static rodent_t rodent = {
 	.device = NULL,
 	.modulename = NULL,
 	.configfile = "/etc/moused.conf",
-	.cfd = 0,
+	.cfd = -1,
+	.mfd = -1,
 	.update = update, /* Modules call moused's update(...) to talk to sysmouse */
 	.logmsg = logmsg, /* Modules call moused's logmsg(...) log stuff */
+
+	.init = NULL,
+	.probe = NULL,
+	.run = NULL,
+
+	.handler = NULL,
 };
 
 int main(int argc, char **argv) {
@@ -94,12 +112,19 @@ int moused(int argc, char **argv) {
 		if (NULL == rodent.probe)
 			logfatal(1, "Module '%s' has no PROBE function?!", rodent.modulename);
 
+		/* Check if this module is the right module for this mouse device */
 		if (rodent.probe(&rodent) == MODULE_PROBE_SUCCESS) {
-			/* MAIN FUNCTION */
-			if (NULL == rodent.run)
+
+			/* Try the RUN method first */
+			//if (NULL != rodent.run)
+				//rodent.run(&rodent);
+			/* else */ if (NULL != rodent.handler)
+				packethandler_main();
+			else 
 				logfatal(1, "Module '%s' has no RUN function?!", rodent.modulename);
 
-			rodent.run(&rodent);
+			/* If we _EVER_ get here, something hs wrong */
+			warnx("Arrived at unexpected location (RUN or PACKETHANDLER method died?)");
 		}
 	} else {
 		warnx("No module selected.");
@@ -134,8 +159,122 @@ static void loadmodule(char *path) {
 	rodent.init = dlsym(handle, "init");
 	rodent.probe = dlsym(handle, "probe");
 	rodent.run = dlsym(handle, "run");
+	rodent.handler = dlsym(handle, "handler");
+	rodent.handler_init = dlsym(handle, "handler_init");
 }
 
-static int update(struct mouse_info *delta) {
-	return ioctl(rodent.cfd, CONS_MOUSECTL, delta);
+static int update(mouse_info_t *delta) {
+	mouse_info_t mydelta;
+
+	/* Store state and copy delta into mydelta if this isn't a no-op */
+	if (NULL != delta) {
+		memcpy(&mydelta, delta, sizeof(mouse_info_t));
+		memcpy(&(rodent.state), delta, sizeof(mouse_info_t));
+	}
+	else {
+		memset(&mydelta, 0, sizeof(mouse_info_t));
+	}
+
+	filter(&mydelta, (NULL == delta) ? 1 : 0);
+	return ioctl(rodent.cfd, CONS_MOUSECTL, &mydelta);
+}
+
+static int state = 0;
+static int distance = 0;
+#define NORMAL 0
+#define SCROLLING 1
+#define SCROLLREADY 2
+#define SCROLLTIMEOUT 3
+
+/* 
+ * This is all the code a filter should have to be 
+ * How should this be implemented?
+ */
+static void filter(mouse_info_t *delta, int noop) {
+	if (noop == 1) {
+		if (state == SCROLLREADY) {
+			warnx("noop");
+			memcpy(delta, &(rodent.state), sizeof(mouse_info_t));
+			dumpstate(delta);
+			state = SCROLLTIMEOUT;
+		}
+	} else {
+		if (delta->u.data.buttons & (1<<1)) {
+			if (state == SCROLLTIMEOUT)
+				return;
+
+			state = SCROLLREADY;
+
+			/* Middle mouse is held */
+			if (delta->u.data.x != 0 || delta->u.data.y != 0) {
+				if (state == SCROLLREADY)
+					state = SCROLLING;
+
+				distance += delta->u.data.y;
+
+#define THRESHOLD 3
+
+				if (distance > THRESHOLD) {
+					delta->u.data.z = 1;
+					distance = 0;
+				} else if (distance < -THRESHOLD) {
+					delta->u.data.z = -1;
+					distance = 0;
+				}
+
+				//if (delta->u.data.x > 4) {
+					//delta->u.data.z += 2;
+				//} else if (delta->u.data.x < 4) {
+					//delta->u.data.z += -2;
+				//}
+
+				/* Do not move! */
+				delta->u.data.x = delta->u.data.y = 0;
+			}
+
+			/* Don't click middle */
+			delta->u.data.buttons &= ~(1<<1);
+		} else {
+			state = NORMAL;
+		}
+	}
+}
+
+static void packethandler_main() {
+	fd_set fds;
+	struct timeval timeout;
+
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 100000; /* 20mec */
+
+	if (NULL != rodent.handler_init)
+		rodent.handler_init(&rodent);
+
+	if (rodent.mfd == -1) {
+		rodent.mfd = open(rodent.device, O_RDWR);
+		if (rodent.mfd == -1) 
+			logfatal(1, "Failed trying to open '%s'", rodent.device);
+	}
+
+	FD_ZERO(&fds);
+	FD_SET(rodent.mfd, &fds);
+	
+	for (;;) {
+		int c;
+
+		c = select(FD_SETSIZE, &fds, NULL, NULL, &timeout);
+		if (c < 0)
+			logfatal(1, "Error with select call");
+		else if (c > 0)
+			rodent.handler(&rodent);
+		else 
+			rodent.update(NULL); /* TIMEOUT HAPPENED */
+
+		FD_SET(rodent.mfd, &fds);
+	}
+}
+
+void dumpstate(mouse_info_t *delta) {
+	warnx("(%d,%d) z:%d b:%08x", delta->u.data.x, delta->u.data.y,
+			delta->u.data.z, delta->u.data.buttons);
 }
